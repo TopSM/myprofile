c72e33e9ace036798aa4188387617cd11d6fcede
TopSM <ostavo.p@gmail.com>
2021-05-17 16:09:03 -0400

    footer update

--- myProfile-master/my-app-page/src/footer.jsx
+++ myProfile-master/my-app-page/src/footer.jsx
@@ -6,35 +6,35 @@ function Footer() {
         <div className="footer" >
             <div>
                 <p>
-                    Site built and mainted by Ostavo Palacios &copy;2020 <br/>
-                    Last revised 11/8/2020
+                    Site built and mainted by Ostavo Palacios &copy;2021 <br />
+                    Last revised 5/17/2021
                 </p>
-                </div>
+            </div>
             <div className="social-icon-footer">
-                <SocialIcon 
+                <SocialIcon
                     url="https://www.linkedin.com/in/ostavo-palacios/"
                     className="social-icon"
                     bgColor=" #800000"
                 />
 
-                
-                <SocialIcon 
+
+                <SocialIcon
                     url="https://github.com/TopSM"
                     className="social-icon"
                     bgColor=" #800000"
-                    />
-                <SocialIcon 
+                />
+                <SocialIcon
                     url="https://www.instagram.com/albumpalace98/"
                     className="social-icon"
                     bgColor=" #800000"
-                    />
-                <SocialIcon 
+                />
+                <SocialIcon
                     url="https://mail.google.com/mail/?view=cm&source=mailto&to=Ostavo.palacios@gmail.com"
                     className="social-icon"
                     bgColor=" #800000"
-                    />
+                />
             </div>
-            
+
         </div>
     );
 }
